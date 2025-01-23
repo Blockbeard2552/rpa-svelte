abc12639bb481984d3426a87d2a387c655f475c0
Blockbeard2552 <joey.stephens524@gmail.com>
2025-01-22 19:53:17 -0500

    JDS-34 User Dashboard.

--- src/routes/private/dashboard/+page.svelte
+++ src/routes/private/dashboard/+page.svelte
@@ -1,14 +1,49 @@
 <script lang="ts">
 	import { getUserState } from '$lib/state/user-state.svelte';
+	import Icon from '@iconify/svelte';
 
 	let userContext = getUserState();
 	let { allBooks } = $derived(userContext);
+	let { userName } = $derived(userContext);
 
 	$inspect(allBooks);
 </script>
 
-<ul>
-	{#each allBooks as book}
-		<li>{book.title}</li>
-	{/each}
-</ul>
+<div class="dashboard">
+	<div class="dashboard-header mb-m">
+		<a href="/private/scan-shelf" class="add-book">
+			<Icon icon="icons8:plus" width={'72'} height={'72'} />
+			<p>Add Book</p>
+		</a>
+		<div class="headline">
+			<h3 class="bold mb-xs">Welcome Back, {userName}</h3>
+			<p>Here is some text.</p>
+		</div>
+	</div>
+	<!-- Book Categories -->
+</div>
+
+<style>
+	.dashboard-header {
+		display: flex;
+		align-items: flex-start;
+		justify-content: space-between;
+		width: 100%;
+	}
+
+	.add-book {
+		display: flex;
+		align-items: center;
+		text-decoration: none;
+	}
+
+	.add-book p {
+		margin-left: 8px;
+	}
+
+	.headline {
+		text-align: right;
+		max-width: 30%;
+		min-width: 300px;
+	}
+</style>
